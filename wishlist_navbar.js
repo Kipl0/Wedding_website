eee6fdd6c895ba6f877b6ffcb17f3ce6de77c3c6
Maja Larsen <maalmaja@gmail.com>
2022-09-23 11:32:27 +0200

    adding js function to scroll on wish-list page for navbar

---
+++ wishlist_navbar.js
@@ -0,0 +1,10 @@
+document.addEventListener("scroll", function () {
+  let vHeight = (window.innerHeight)*0.98
+  const navbar = document.querySelector(".wish_list_subject_finder_container");
+  const navbarHeight = vHeight;
+  const distanceFromTop = Math.abs(
+    document.body.getBoundingClientRect().top
+  );
+  if (distanceFromTop >= navbarHeight) navbar.classList.add("fixed-top");
+  else navbar.classList.remove("fixed-top");
+});
